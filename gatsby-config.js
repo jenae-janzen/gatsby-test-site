568da855167f9137ab4e10a04783d3dcfcecba69
Jenae Janzen <jenae.janzen@netlify.com>
2023-10-16 15:57:30 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `gatsby test site`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
