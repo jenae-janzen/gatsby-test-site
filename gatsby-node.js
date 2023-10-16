b0442713a1274560760b7879a6e63a5cb88c7195
Jenae Janzen <jenae.janzen@netlify.com>
2023-10-16 16:46:15 -0400

    create page

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,8 @@
-createRedirect({
-  fromPath: `/page1`,
-  toPath: `/page2`,
-})
+exports.createPages = async ({ actions }) => {
+  const { createRedirect } = actions
+
+  createRedirect({
+    fromPath: `/page1`,
+    toPath: `/page2`,
+  })
+}
